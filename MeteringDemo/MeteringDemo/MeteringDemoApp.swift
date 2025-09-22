a0b17c6f3ccd34cce52d4e3bdc254dd3e046060b
BoMin <bmlee@postech.ac.kr>
2025-09-22 19:22:54 +0900

    Feat: MeteringDemo Project

---
+++ MeteringDemo/MeteringDemo/MeteringDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteringDemoApp.swift
+//  MeteringDemo
+//
+//  Created by BoMin Lee on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MeteringDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
